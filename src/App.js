0920b97026298acd55a912b673e7e0059526b7f1
Bhawna <bhawna.singh3022@gmail.com>
2023-03-22 23:42:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Homepage from './components/homepage/Homepage'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from './components/footer/Footer';
 
 
 const App = () => {
   return (
     <Router>
       <Navbar/>
+  
       <Routes>
+        
         <Route path="/" exact element={<Homepage/>}/>
       </Routes>
+      <Footer />
     </Router>
+    
   )
 }
 
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import"./Footer.css";
+import React from 'react'
+import "./Footer.css"
 
 const Footer = () => {
   return (
-    
-      
     <div className="foot-container">
-      <div className="Footer">
     
-    <div className="footerRow">
-      <div className="f_nav">
-     
-        <h3 className="textfooter">Recycle</h3>
-        </div>
+    <div className="Footer">
+    
+      <div className="footerRow">
+        <div className="f_nav">
        
+          <h3 className="textfooter">Recyclo</h3>
+          <h3 className="text-footer">Privacy Policy</h3>
+          
+          </div>
+          </div>
+    
       <div className="row copyright">© Copyright. AllRightsReserved</div>
-      </div>
-      </div>
-      </div>
+        
+    </div>
+    </div>
   )
 }
 
 export default Footer
+
+        
+
+      
+ 
