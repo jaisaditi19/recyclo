28c0e145d02ce4beb0d745cfb26e6eb5026784e8
Bhawna <bhawna.singh3022@gmail.com>
2023-03-23 11:00:24 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/navbar/Navbar'
 import Homepage from './components/homepage/Homepage'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/footer/Footer';
+import Registration from './components/RegistrationForm/Registration';
+
 
 
 const App = () => {
@@ -11,10 +13,12 @@ const App = () => {
       <Navbar/>
   
       <Routes>
+        <Route path="/register" exact element={<Registration/>}/>
         
         <Route path="/" exact element={<Homepage/>}/>
       </Routes>
       <Footer />
+      
     </Router>
     
   )
---
+++ src/components/RegistrationForm/Registration.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+function  Registration()  {
+    return (
+  <>
+      
+          <form>
+              <div className="form_wrap">
+                  <div className="input_grp">
+                      <div class="input_wrap">
+                          <label for="fname">First Name</label>
+                          <input type="text" id="fname" placeholder='Enter Your fname'/>
+                      </div>
+                      <div className="input_wrap">
+                          <label for="lname">Last Name</label>
+                          <input type="text" id="lname" placeholder='Enter Your lname' />
+                      </div>
+                  </div>
+                  </div>
+                  </form>
+                </>
+                  
+
+                  )
+                  };
+                  
+                
+export default Registration
